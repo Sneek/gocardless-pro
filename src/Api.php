692130b2d1729a40abeb36ed11d8d0a0db4acc06
Toyin <toyin.butler@gmail.com>
2015-06-14 10:11:27 +0100

    Added Subscriptions support: Ability to create, list, get single, update and cancel subscriptions.

--- src/Api.php
+++ src/Api.php
@@ -14,6 +14,7 @@ use GoCardless\Pro\Models\CustomerBankAccount;
 use GoCardless\Pro\Models\Mandate;
 use GoCardless\Pro\Models\Payment;
 use GoCardless\Pro\Models\RedirectFlow;
+use GoCardless\Pro\Models\Subscription;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\BadResponseException;
 
@@ -27,6 +28,7 @@ class Api
     const PAYMENTS               = 'payments';
     const REDIRECT_FLOWS         = 'redirect_flows';
     const HELPERS                = 'helpers';
+    const SUBSCRIPTIONS          = 'subscriptions';
 
     const SANDBOX_URL    = 'https://api-sandbox.gocardless.com/';
     const PRODUCTION_URL = 'https://api.gocardless.com/';
@@ -528,6 +530,78 @@ class Api
 
         return Payment::fromArray($response);
     }
+
+    /**
+     * @see https://developer.gocardless.com/pro/#subscriptions-create-a-subscription
+     *
+     * @param Subscription $subscription
+     *
+     * @return Subscription
+     */
+    public function createSubscription(Subscription $subscription)
+    {
+        $response = $this->post(self::SUBSCRIPTIONS, $subscription->toArray());
+
+        return Subscription::fromArray($response);
+    }
+
+    /**
+     * @see https://developer.gocardless.com/pro/#subscriptions-list-subscriptions
+     *
+     * @param array $options
+     *
+     * @return array
+     */
+    public function listSubscriptions($options = [])
+    {
+        $response = $this->get(self::SUBSCRIPTIONS, $options);
+
+        return $this->buildCollection(new Subscription, $response);
+    }
+
+    /**
+     * @see https://developer.gocardless.com/pro/#subscriptions-get-a-single-subscription
+     *
+     * @param $id
+     *
+     * @return Subscription
+     */
+    public function getSubscription($id)
+    {
+        $response = $this->get(self::SUBSCRIPTIONS, [], $id);
+
+        return Subscription::fromArray($response);
+    }
+
+
+    /**
+     * @see https://developer.gocardless.com/pro/#subscriptions-update-a-subscription
+     *
+     * @param Subscription $subscription
+     *
+     * @return Subscription
+     */
+    public function updateSubscription(Subscription $subscription)
+    {
+        $response = $this->put(self::SUBSCRIPTIONS, $subscription->toArrayForUpdating(), $subscription->getId());
+
+        return Subscription::fromArray($response);
+    }
+
+    /**
+     * @see https://developer.gocardless.com/pro/#subscriptions-cancel-a-subscription
+     *
+     * @param $id
+     *
+     * @return Subscription
+     */
+    public function cancelSubscription($id)
+    {
+        $response = $this->post(self::SUBSCRIPTIONS, [], $id . '/actions/cancel');
+
+        return Subscription::fromArray($response);
+    }
+
 
     /**
      * @see https://developer.gocardless.com/pro/#redirect-flows-create-a-redirect-flow
